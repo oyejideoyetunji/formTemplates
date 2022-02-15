745705f5a7e56af5fbf78f87d923bdada5d186a5
oluwatobi oyejide <moyejide2016@gmail.com>
2022-02-15 22:35:51 +0100

    Updated readme

--- src/store/index.ts
+++ src/store/index.ts
@@ -3,13 +3,7 @@ import { TypedUseSelectorHook, useDispatch, useSelector } from "react-redux";
 import rootReducer, { RootState } from "./rootReducers";
 
 
-const store = configureStore({
-    reducer:    rootReducer,
-    middleware: getDefaultMiddleware => {
-        const [thunk, immutableStateInvariant] = getDefaultMiddleware()
-        return [thunk, immutableStateInvariant]
-    },
-})
+const store = configureStore({ reducer: rootReducer })
 type AppDispatch = typeof store.dispatch
 
 
